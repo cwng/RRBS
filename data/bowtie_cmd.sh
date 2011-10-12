750a988d5518d830d11ac0e04c79f8f915d5befd
Adam Labadorf <alabadorf@gmail.com>
2011-10-12 18:47:27 -0400

    added commands for running bowtie and rmap

---
+++ data/bowtie_cmd.sh
@@ -0,0 +1,1 @@
+bowtie -p 16 -5 1 -l 20 -n 2 -k 2 --best --strata --norc --phred64-quals -S /home/labadorf/analysis/RRBS/annot/mm9_msp1/mm9_msp1.ebwt 110929_RRBS111.fastq 110929_RRBS111_align.sam
---
+++ data/rmap_cmd.sh
@@ -0,0 +1,1 @@
+/home/labadorf/software/rmap_v2.05/src/mappers/rmapbs/rmapbs -o 110929_RRBS111_rmapbs_aligns.out -F chroms.txt -B -v 110929_RRBS111.fasta
