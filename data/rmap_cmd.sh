e14553a90e3b2c2b6cb562e5ab76cf6cf556984b
Adam Labadorf <alabadorf@gmail.com>
2011-10-13 17:48:57 -0400

    added rmapbs command for q7

--- data/rmap_cmd.sh
+++ data/rmap_cmd.sh
@@ -1,1 +1,2 @@
-/home/labadorf/software/rmap_v2.05/src/mappers/rmapbs/rmapbs -o 110929_RRBS111_rmapbs_aligns.out -F chroms.txt -B -v 110929_RRBS111.fasta
+#/home/labadorf/software/rmap_v2.05/src/mappers/rmapbs/rmapbs -o 110929_RRBS111_rmapbs_aligns.out -F chroms.txt -B -v 110929_RRBS111.fasta
+/home/labadorf/software/rmap_v2.05/src/mappers/rmapbs/rmapbs -o 110929_RRBS7_rmapbs_aligns.out -F chroms.txt -B -v 110929_RRBS7.fasta
